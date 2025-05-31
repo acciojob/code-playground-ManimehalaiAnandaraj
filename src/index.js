35e028f4c57a6f5eb21026f52d971c5650d4ad94
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-31 20:28:55 +0530

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 
 
 
-ReactDOM.render(<BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+ReactDOM.render(
+
+    <App />,
+   
   document.getElementById("root")
 );
